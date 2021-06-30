585a62af3d86a53ff12c1a67c481980185b42a67
ConcoleFirst <alek.kalashnikov32@yandex.ru>
2021-06-30 18:54:43 +0300

    ++

--- src/com/company/Task8_12.java
+++ src/com/company/Task8_12.java
@@ -9,18 +9,22 @@ public class Task8_12 {
         int program = 3;
         int scores[][] = new int[athlete][program];
         int sumOfAthlete[] = new int[athlete];
-        int sumOfPointsAthlete =0;
+        int sumOfPointsAthlete=0;
+        int sumOfProgram[] = new int[program];
+        int sumOfPointsProgram=0;
 
         Random random = new Random();
 
         for (int i = 0; i < athlete; i++) {
             for (int j = 0; j < program; j++) {
+
                 scores[i][j] = random.nextInt(10) + 1;
             }
         }
 
         for (int i = 0; i < athlete; i++) {
             for (int j = 0; j < program; j++) {
+
                 System.out.printf("%-5d", scores[i][j]);
             }
             System.out.println();
@@ -31,12 +35,13 @@ public class Task8_12 {
         System.out.println();
 
         for (int i = 0; i < athlete; i++) {
-            sumOfAthlete[i] = 0;
 
+            sumOfAthlete[i] = 0;
         }
 
         for (int i = 0; i < athlete; i++) {
             for (int j = 0; j < program; j++) {
+
                 sumOfAthlete[i] += scores[i][j];
                 sumOfPointsAthlete += sumOfAthlete[i];
             }
@@ -53,5 +58,33 @@ public class Task8_12 {
             System.out.println("Среднее количество баллов спортсмена = " +avgAthlete);
         }
 
+
+        // TODO: 30.06.2021 average score for each program
+        System.out.printf("++++++++++++++++++++");
+        System.out.println();
+
+        for (int j = 0; j < program; j++) {
+
+            sumOfProgram[j] = 0;
+        }
+
+        for (int i = 0; i < athlete; i++) {
+            for (int j = 0; j < program; j++) {
+
+                sumOfProgram[j] += scores[i][j];
+                sumOfPointsProgram += sumOfProgram[j];
+            }
+        }
+
+        for (int j = 0; j < program; j++) {
+
+            System.out.printf("%-5d", sumOfProgram[j]);
+
+            sumOfPointsProgram += sumOfProgram[j];
+            System.out.println(" - сумма баллов за программу № " + (j + 1));
+
+            double avgProgram = (double) (sumOfProgram[j] / (double) program);
+            System.out.println("Среднее количество баллов за программу = " + avgProgram);
+        }
     }
 }
